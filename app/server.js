93b6c0ec019cb26b65d327f7ca558d0a55c3e1fe
Kishore <kishore97bala@gmail.com>
2025-09-21 21:18:16 +0530

    8th

--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
 const http = require("http");
-
-// ✅ Use port 3000 to match your Kubernetes Service
-const port = 3000;
+const port = 80;
 
 http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello from EKS test-app!\n");
 })
-// ✅ Bind to 0.0.0.0 so it listens on all interfaces (not just localhost)
 .listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
